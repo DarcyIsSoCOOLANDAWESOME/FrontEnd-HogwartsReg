77e6ef3ac6373e329b75b1f0c32074918af6e106
‘darcy’ <darcy.mears@yahoo.com>
2026-06-04 16:54:33 +0100

    Adding a function when the enrol button is pressed, the information is saved

--- register.js
+++ register.js
@@ -1,1 +1,7 @@
-document.getElementById("enrol").submit();
+// document.getElementById("enrol").submit();
+
+document.querySelectorAll("#regForm input");
+Array.from(document.querySelectorAll("#regForm input")).reduce(
+  (acc, input) => ({ ...acc, [input.id]: input.value }),
+  {},
+);
