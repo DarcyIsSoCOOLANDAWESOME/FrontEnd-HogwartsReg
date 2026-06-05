6d089fdb5a88b0e70b27b7465abb68eecec91c56
‘darcy’ <darcy.mears@yahoo.com>
2026-06-05 10:28:23 +0100

    Enrol button doesn't refresh page. There is a function that saves the information inputted

--- register.js
+++ register.js
@@ -1,7 +1,7 @@
-// document.getElementById("enrol").submit();
-
-document.querySelectorAll("#regForm input");
-Array.from(document.querySelectorAll("#regForm input")).reduce(
-  (acc, input) => ({ ...acc, [input.id]: input.value }),
-  {},
-);
+document.getElementById("regForm").addEventListener("submit", function (e) {
+  e.preventDefault(); //prevent default refresh behaviour, so
+  const formData = Array.from(
+    document.querySelectorAll("#regForm input, #regForm select"),
+  ).reduce((acc, field) => ({ ...acc, [field.id]: field.value }), {});
+  console.log("Ready for Spring Boot:", formData);
+});
