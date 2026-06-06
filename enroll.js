833d34c959b429a1464ed8fd59a0a68c93eed8bd
Ihsan Kabir <i.kabir1210@gmail.com>
2026-06-06 19:27:48 +0100

    added crud methods

--- enroll.js
+++ enroll.js
@@ -2,9 +2,12 @@ const optionBtns = document.querySelectorAll(".option");
 const counterText = document.getElementById('numSelected');
 const submitBtn = document.querySelector("#submitBtn")
 
-
 let selectedCourses = []
+const studentId = "idk lol"
 
+//const apiURL = idk bro, backend gotta lmk fam
+
+submitBtn.disabled = true; 
 
 optionBtns.forEach ( btn => { 
     btn.addEventListener("click", (e) => {
@@ -14,17 +17,116 @@ optionBtns.forEach ( btn => {
         
         if (selectedCourses.length < 3) {
         selectedCourses.push(userChoice);
-        }
+            if (selectedCourses.length == 3) {
+                submitBtn.disabled = false;
+            } //check if 3
+        } //push if under 3
 
         console.log(selectedCourses)
 
-
     } //close e
-
-
-)
+) //close eL
 
 } //close fE func
 
-
 ) //close forEach
+
+
+submitBtn.addEventListener("click", (e) => {
+
+    const data = {studentID: studentId, 
+        course1: selectedCourses[0], 
+        course2: selectedCourses[1], 
+        course3: selectedCourses[2] }
+
+        console.log(data);
+        postData(data);
+
+}) //close e and eL
+
+
+//CRUD Methods
+
+const postData = async(data) => {
+
+    const apiURL = "https://domain.com/path/"
+    const params = {
+        method: "POST",
+        body: JSON.stringify(data), //ensures data/body is json format
+        headers: {"Content-Type": "application/json; charset=UTF-8"} //telling API that its indeed json
+    }
+   
+    try { //from docs
+    const response = await fetch(apiURL, params);
+    const result = await response.json();
+    console.log("Success:", result);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+
+  return result
+}
+
+const postData2 = (data) => {
+   
+    const apiURL = "https://domain.com/path/"
+    const params = {
+        method: "POST",
+        body: JSON.stringify(data), //ensures data/body is json format
+        headers: {"Content-Type": "application/json; charset=UTF-8"} //telling API that its indeed json
+    }
+
+    const results = fetch(apiURL, params) //code from freecodecamp or smth
+    .then((response) => response.json()) // jsoning the response 
+    .then((json) => console.log(json))  //probs not needed
+    .catch ((error) => console.log("error:", error))
+
+    return results
+}
+
+
+const getData = async(apiURL) => {
+    
+    try {
+    const fetched = fetch(apiURL)
+    const awaited = await fetched
+    const jsonified = await awaited.json()
+    } catch (error) {
+        console.log("Error occured: "+ error);
+    }
+    //## code needs crazy checking
+
+    return jsonified;
+}
+
+const updateCourses = () => {
+    const apiURL = "https://domain.com/path/"
+    const params = {
+        method: "PUT",
+        body: JSON.stringify(data), //ensures data/body is json format
+        headers: {"Content-Type": "application/json; charset=UTF-8"} //telling API that its indeed json
+    }
+
+    const results = fetch(apiURL, params)
+    .then((response) => response.json()) // jsoning the response 
+    .then((json) => console.log(json))  //probs not needed
+    .catch ((error) => console.log("error:", error))
+
+    return results
+}
+
+
+const deleteCourseData = async(id) => {
+    const apiURL = "https://domain.com/path/{id}"
+    const params = {method:"DELETE"}
+
+    try {
+    const response = await fetch(apiURL, params)
+    } catch (err) {
+        console.log("errors again tbh,",err);
+        
+    }
+
+    return response
+
+}
